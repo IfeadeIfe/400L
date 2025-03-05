c3e27d92b5a6e3333af8a3a1fa255f709e49f4f6
IfeadeIfe <ifedayoadeife763@gmail.com>
2025-03-05 01:19:16 +0100

    Updated API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const handlePredict = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://four00l-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
